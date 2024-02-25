150ea43c9bc194e6d14b59c9784df27cf64946ef
frankachu <108089618+frankachu@users.noreply.github.com>
2024-02-26 01:27:38 +0300

    1part

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 
-# Homework1
+# Homework2
 
 ## Description
-I create page using html and css. There is some information about me, images with Pokemon and stars. Also you can see the time that you access to this page.
-1. url_for() - helps to serve static components 
-2. render_template(template, **parametrs) - this method renders out template with specified parameters. In this work parameters are access time and student name. 
-3. redirect() - is used to redirect from url (/) to (/profile)
-   ![image](https://github.com/frankachu/Elizaveta_Polukhina_1lab/assets/108089618/efa52c27-862e-42f0-8be9-b248eafb3a15)
+The project implements user authentication in the form via the POST method. Upon successful authorization, the username is recorded in the session and the user profile is displayed. 
+
+When you log out of your account, the session is cleared.
+
+If the input is unsuccessful, the user is prompted to re-enter the authorization data.
+
+If the user is not logged in, they cannot get into the profile, as they are redirected to the authentication page.
+
+Usernames and passwords are stored in MongoDB.
 
 
-Also you can look at this site that was posted via Git Pages: https://frankachu.github.io/
-Unfornatelly, this version doesn't show the time access.
+
 
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, session
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 app = Flask(__name__)
+app.secret_key = b'^9p8A(h]bpJ)4nThp*b9'
 db = client.wad_users
 
 def create(username, password):
@@ -11,22 +12,49 @@ def create(username, password):
             "username": username,
             "password": password
         })
-    print("Добавлен")
+
+def check_user():
+    return 'username' in session
 
 @app.route('/profile')
 def profile():
-    name="Elizaveta Polukhina"
-    time = datetime.now()
-    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    return render_template('index.html', name=name, time=formatted_time)
+    if not check_user():
+        return redirect(url_for('login'))
+    username = session['username']
+    return render_template('profile.html', username=username)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return redirect(url_for('profile'))
+    if check_user():
+        return redirect(url_for('profile'))
+    else:
+        return render_template("index.html")
 
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if check_user():
+        return redirect(url_for('profile'))
+
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = db.user.find_one({'username': username, 'password': password})
+        if user:
+            session['username'] = username
+            return redirect(url_for('profile'))
+        else:
+            return render_template('index.html', error='Invalid username or password')     
+    else:
+        return render_template("index.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True)
 
     
      
